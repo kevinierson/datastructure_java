fbdeb956ce729757a59a6dfaf25b3683f3a1451e
kevinierson <1538281872@qq.com>
2021-02-19 17:15:38 +0800

    归并排序

---
+++ src/kevin/datastructure/insertionsort/BinaryInsertionSort.java
@@ -0,0 +1,40 @@
+package kevin.datastructure.insertionsort;
+
+import java.util.Arrays;
+
+/**
+ * @author: kevin
+ * @date: 9:11 2021/2/19
+ */
+@SuppressWarnings("all")
+public class BinaryInsertionSort {
+
+    public static void main(String[] args) {
+        int[] arr = {3,44,38,5,47,15,36,26,27,2,46,4,19,50,48};
+        System.out.println("排序前：" + Arrays.toString(arr));
+        int[] result = binaryInsertionSort(arr);
+        System.out.println("排序后：" + Arrays.toString(result));
+
+    }
+
+    private static int[] binaryInsertionSort(int[] arr) {
+        for(int i = 1; i < arr.length; ++i){
+            int insertValue = arr[i];
+            int low = 0;
+            int high = i - 1;
+            while(low <= high){
+                int mid = (low + high) / 2;
+                if(insertValue < arr[mid])
+                    high = mid - 1;
+                else
+                    low =mid + 1;
+            }
+            for(int j = i - 1; j >= high + 1; --j){
+                arr[j + 1] = arr[j];
+            }
+            arr[high + 1] = insertValue;
+        }
+        return arr;
+    }
+
+}
--- src/kevin/datastructure/mergesort/MergeSort.java
+++ src/kevin/datastructure/mergesort/MergeSort.java
@@ -17,45 +17,58 @@ public class MergeSort {
     }
 
     public static void mergeSort(int[] arr) {
-        int[] temp = new int[arr.length];
+        int[] temp = new int[arr.length];//辅助空间
         mergeSort(arr, 0, arr.length - 1, temp);
     }
 
+    //参数分别为 待排序数组，左指针，右指针，辅助数组
     public static void mergeSort(int[] arr, int left, int right, int[] temp) {
-        //参数分别为 待排序数组，左指针，有指针，辅助数组
-        //因为使用了递归，所以我们必须规定递归条件否则将进行无线循环
+        //因为使用递归，所以必须规定递归条件，否则将进行无线循环
         if (left < right) {
             //将数组进行分割
-            int mid = (left + right) / 2;
-            //对左子数组继续进行归并排序
+            int mid = (left + right) / 2; //中间索引
+            //向左递归进行分解
             mergeSort(arr, left, mid, temp);
-            //对右子数组继续进行归并排序
+            //向右递归进行分解
             mergeSort(arr, mid + 1, right, temp);
-            //将数组进行合并
+            //每分解一轮便合并一轮
             Merge(arr, left, mid, right, temp);
         }
-
     }
 
-    //合并函数
+    /**
+     * 合并方法
+     * @param arr   待排序的数组
+     * @param left  左边有序序列的初始索引
+     * @param mid   中间索引
+     * @param right 右边有序序列的初始索引
+     * @param temp  辅助数组
+     */
     public static void Merge(int[] arr, int left, int mid, int right, int[] temp) {
-        int i = left;
-        int j = mid + 1;
-        //t为辅助数组的索引
-        int t = 0;
+        int i = left;// 初始化i，左边有序序列的初始索引
+        int j = mid + 1;// 初始化j，右边有序序列的初始索引(右边有序序列的初始位置即为中间位置的后一个位置)
+        int t = 0;//t为辅助数组的索引
+
+        // 先把左右两边的数据(已经有序)按规则填充到temp数组
+        // 直到左右两边的有序序列，有一边处理完成为止
         while (i <= mid && j <= right) {
-            //当二者都没有到达最后一位时，进行比较并向辅助数组复制
+            // 如果左边有序序列的当前元素小于或等于右边有序序列的当前元素，就将左边的元素填充到temp数组中
             if (arr[i] < arr[j]) {
                 temp[t++] = arr[i++];
             } else {
+                // 反之，将右边有序序列的当前元素填充到temp数组中
                 temp[t++] = arr[j++];
             }
         }
-        //当其中一个数组复制完毕后，将另一个数组内的数组全部复制进辅助数组
+        // 把有剩余数据的一边的元素填充到temp中
         while (i <= mid) {
+            // 此时说明左边序列还有剩余元素
+            // 全部填充到temp数组
             temp[t++] = arr[i++];
         }
         while (j <= right) {
+            // 此时说明左边序列还有剩余元素
+            // 全部填充到temp数组
             temp[t++] = arr[j++];
         }
         t = 0;
@@ -63,6 +76,5 @@ public class MergeSort {
         while (left <= right) {
             arr[left++] = temp[t++];
         }
-
     }
 }
